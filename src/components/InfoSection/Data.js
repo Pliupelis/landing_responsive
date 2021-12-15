3342972dff7c91ecbc1de5bd03ee82d2add7a315
pliupelis <pliupelis@inbox.lt>
2021-12-15 20:17:36 +0200

    reusable section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,75 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/car.svg").default,
+  alt: "Car",
+  dark: true,
+  dark2: false,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/wallet.svg").default,
+  alt: "Wallet",
+  dark: false,
+  dark2: true,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "services",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/office.svg").default,
+  alt: "Car",
+  dark: false,
+  dark2: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjFour = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/benefits.svg").default,
+  alt: "Wallet",
+  dark: false,
+  dark2: true,
+  primary: false,
+  darkText: true,
+};
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
 import HeroSection from "../heroSection";
+import InfoSection from "../InfoSection";
+import { homeObjOne } from "../InfoSection/Data";
+import { homeObjTwo } from "../InfoSection/Data";
+import { homeObjThree } from "../InfoSection/Data";
+import { homeObjFour } from "../InfoSection/Data";
+
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   console.log(isOpen);
@@ -15,6 +21,10 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjFour} />
     </>
   );
 };
